dbdb42b2b279057a3e4c16273fa81ce4dbdbfc12
Brian <briantechdom@gmail.com>
2017-07-13 15:41:40 -0700

    Added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,15 @@ var sass = require('gulp-sass');
 //requires gulp from package.json
 var browserSync = require('browser-sync');
 var reload = browserSync.reload; //sub-method from browserSync
+var autoprefixer = require('gulp-autoprefixer');
 
-var SOURCEPATHS = {
 
-  sassSource : 'src/scss/*.scss'  //will check for any file that has .scss extension
+var SOURCEPATHS = {  //src folder where changes are initially made
+
+  sassSource : 'src/scss/*.scss',  //will check for any file that has .scss extension
+  htmlSource: 'src/*.html'
 }
-var APPPATH = {
+var APPPATH = {  //app folder where final app is located
   root : 'app/',
   css : 'app/css',
   js : 'app/js'
@@ -18,11 +21,19 @@ var APPPATH = {
 //tasks method used for gulp
 gulp.task('sass', function(){
   return gulp.src(SOURCEPATHS.sassSource)
+    //autoprefixer
+    .pipe(autoprefixer())
     //The gulp task sass will pupe the src/scss/app.scss file
     //changes into the app/css/app.css file
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    //output can be compressed or minified instead of expanded
     .pipe(gulp.dest(APPPATH.css));
 
+});
+
+gulp.task('copy', function() {
+  gulp.src(SOURCEPATHS.htmlSource)
+    .pipe(gulp.dest(APPPATH.root))
 });
 
 gulp.task('serve', ['sass'], function() {
@@ -34,10 +45,11 @@ gulp.task('serve', ['sass'], function() {
   //initializes browserSync
 });
 
-gulp.task('watch', ['serve', 'sass'], function () {
+gulp.task('watch', ['serve', 'sass', 'copy'], function () {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 
-})
+});
 
 gulp.task('default',['watch'] );
 //['*task', 'task2', ...] tasks will run with defult gulp command
